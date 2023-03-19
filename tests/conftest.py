491fec89b043a2e7232674a30997a19949f56ac4
test@google.com <mohammad.m@routetitan.com>
2023-03-20 01:59:57 +0400

    eighth

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,13 +4,14 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.chrome import ChromeType
 
 
 @pytest.fixture
 def init_driver(request):
     browser_name = request.config.getoption("--browser")
     global driver, driver_options
-    if browser_name in ("chrome", "chrome_headless"):
+    if browser_name in "chrome":
         driver_options = webdriver.ChromeOptions()
         service = ChromeService(executable_path=ChromeDriverManager().install())
         driver = webdriver.Chrome(service=service, options=driver_options)
@@ -18,7 +19,10 @@ def init_driver(request):
         driver_options = webdriver.FirefoxOptions()
         service = FirefoxService(executable_path=GeckoDriverManager().install())
         driver = webdriver.Firefox(service=service, options=driver_options)
-    if browser_name == "chrome_headless":
+    elif browser_name == "chrome_headless":
+        driver_options = webdriver.ChromeOptions()
+        service = ChromeService(executable_path=ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+        driver = webdriver.Chrome(service=service, options=driver_options)
         driver_options.add_argument("--headless")
         driver_options.add_argument("--disable-dev-shm-usage")
         driver_options.add_argument("--no-sandbox")
